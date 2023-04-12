fd0b34d1718a3fccea8ea8c2b9406dd93e061415
Daniel Adeboye <oluwadaunsid@gmail.com>
2023-04-13 00:59:13 +0100

    Implements crud operations for bookstore app

---
+++ pkg/config/app.go
@@ -0,0 +1,29 @@
+package config
+
+import (
+	"github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+
+)
+
+var (
+	db *gorm.DB
+)
+
+func Connect() {
+
+
+	
+
+	d, err := gorm.Open("mysql", "user:password@/dbname?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		panic(err)
+	}
+	db = d
+}
+
+func GetDB() *gorm.DB {
+	return db
+}
